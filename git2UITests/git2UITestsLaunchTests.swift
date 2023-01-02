e0bc68fa8a04177c7e5174ab0e69227c9f18e441
edison <edisonjoao687@gmail.com>
2023-01-02 18:19:10 -0500

    Initial Commit

---
+++ git2UITests/git2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  git2UITestsLaunchTests.swift
+//  git2UITests
+//
+//  Created by Edison Espinosa on 1/2/23.
+//
+
+import XCTest
+
+final class git2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
